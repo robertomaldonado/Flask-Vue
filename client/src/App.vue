a8afef8ff4967e53fae74ab7faee086ec19865cf
Roberto Maldonado <rob@Robertos-MacBook-Pro.local>
2020-06-22 12:15:53 -0400

    flask-vue default

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// import Ping from './components/Ping.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // Ping,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
